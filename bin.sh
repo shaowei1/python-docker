e4989573b179026298bb7ea53912d54291a23a0b
shaowei <shaowei_8910@163.com>
2023-05-09 18:26:03 +0800

    build by python3.10 Task Solution Note

---
+++ bin.sh
@@ -0,0 +1,2 @@
+docker build -f Dockerfile-alpine -t registry.cn-beijing.aliyuncs.com/stormshao/python:latest .
+docker push registry.cn-beijing.aliyuncs.com/stormshao/python:latest
---
+++ bin2.sh
@@ -0,0 +1,2 @@
+docker build -f Dockerfile-alpine2 -t registry.cn-beijing.aliyuncs.com/stormshao/python3.10:latest .
+docker push registry.cn-beijing.aliyuncs.com/stormshao/python3.10:latest
