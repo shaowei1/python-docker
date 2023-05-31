4630d448eb5b37fa927c766f3766bfade450cede
shaowei <shaowei_8910@163.com>
2023-06-01 05:56:55 +0800

    change build Task Solution Note

--- bincv.sh
+++ bincv.sh
@@ -1,2 +1,4 @@
-docker build -f Dockerfile-cv -t registry.cn-beijing.aliyuncs.com/stormshao/python_cv2:latest .
-docker push registry.cn-beijing.aliyuncs.com/stormshao/python_cv2:latest
+# docker build -f Dockerfile-cv -t registry.cn-beijing.aliyuncs.com/stormshao/python_cv2:latest .
+# docker push registry.cn-beijing.aliyuncs.com/stormshao/python_cv2:latest
+docker build -f t-Dockerfile-cv -t registry.cn-beijing.aliyuncs.com/stormshao/python_cv2:v1.0 .
+docker push registry.cn-beijing.aliyuncs.com/stormshao/python_cv2:v1.0
